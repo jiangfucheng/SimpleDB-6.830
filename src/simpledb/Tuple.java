c8772e487940d42cea109565ce45ace34a644813
jiangfucheng <1678179319@qq.com>
2019-12-06 22:20:53 +0800

    implement Tuple and TupleDesc

--- src/simpledb/Tuple.java
+++ src/simpledb/Tuple.java
@@ -1,8 +1,9 @@
 package simpledb;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -11,97 +12,97 @@ import java.util.Iterator;
  */
 public class Tuple implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Create a new tuple with the specified schema (type).
-     *
-     * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
-     */
-    public Tuple(TupleDesc td) {
-        // some code goes here
-    }
+	private TupleDesc tupleDesc;
 
-    /**
-     * @return The TupleDesc representing the schema of this tuple.
-     */
-    public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
-    }
+	private List<Field> fields;
 
-    /**
-     * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
-     */
-    public RecordId getRecordId() {
-        // some code goes here
-        return null;
-    }
+	private RecordId recordId;
 
-    /**
-     * Set the RecordId information for this tuple.
-     *
-     * @param rid
-     *            the new RecordId for this tuple.
-     */
-    public void setRecordId(RecordId rid) {
-        // some code goes here
-    }
+	/**
+	 * Create a new tuple with the specified schema (type).
+	 *
+	 * @param td the schema of this tuple. It must be a valid TupleDesc
+	 *           instance with at least one field.
+	 */
+	public Tuple(TupleDesc td) {
+		this.tupleDesc = td;
+		this.fields = new ArrayList<>();
+	}
 
-    /**
-     * Change the value of the ith field of this tuple.
-     *
-     * @param i
-     *            index of the field to change. It must be a valid index.
-     * @param f
-     *            new value for the field.
-     */
-    public void setField(int i, Field f) {
-        // some code goes here
-    }
+	/**
+	 * @return The TupleDesc representing the schema of this tuple.
+	 */
+	public TupleDesc getTupleDesc() {
+		// some code goes here
+		return this.tupleDesc;
+	}
 
-    /**
-     * @return the value of the ith field, or null if it has not been set.
-     *
-     * @param i
-     *            field index to return. Must be a valid index.
-     */
-    public Field getField(int i) {
-        // some code goes here
-        return null;
-    }
+	/**
+	 * @return The RecordId representing the location of this tuple on disk. May
+	 * be null.
+	 */
+	public RecordId getRecordId() {
+		return this.recordId;
+	}
 
-    /**
-     * Returns the contents of this Tuple as a string. Note that to pass the
-     * system tests, the format needs to be as follows:
-     *
-     * column1\tcolumn2\tcolumn3\t...\tcolumnN
-     *
-     * where \t is any whitespace (except a newline)
-     */
-    public String toString() {
-        // some code goes here
-        throw new UnsupportedOperationException("Implement this");
-    }
+	/**
+	 * Set the RecordId information for this tuple.
+	 *
+	 * @param rid the new RecordId for this tuple.
+	 */
+	public void setRecordId(RecordId rid) {
+		this.recordId = rid;
+	}
 
-    /**
-     * @return
-     *        An iterator which iterates over all the fields of this tuple
-     * */
-    public Iterator<Field> fields()
-    {
-        // some code goes here
-        return null;
-    }
+	/**
+	 * Change the value of the ith field of this tuple.
+	 *
+	 * @param i index of the field to change. It must be a valid index.
+	 * @param f new value for the field.
+	 */
+	public void setField(int i, Field f) {
+		if (i == fields.size()) {
+			fields.add(f);
+		} else {
+			fields.set(i, f);
+		}
+	}
 
-    /**
-     * reset the TupleDesc of this tuple (only affecting the TupleDesc)
-     * */
-    public void resetTupleDesc(TupleDesc td)
-    {
-        // some code goes here
-    }
+	/**
+	 * @param i field index to return. Must be a valid index.
+	 * @return the value of the ith field, or null if it has not been set.
+	 */
+	public Field getField(int i) {
+		return fields.get(i);
+	}
+
+	/**
+	 * Returns the contents of this Tuple as a string. Note that to pass the
+	 * system tests, the format needs to be as follows:
+	 * <p>
+	 * column1\tcolumn2\tcolumn3\t...\tcolumnN
+	 * <p>
+	 * where \t is any whitespace (except a newline)
+	 */
+	public String toString() {
+		// some code goes here
+		throw new UnsupportedOperationException("Implement this");
+	}
+
+	/**
+	 * @return An iterator which iterates over all the fields of this tuple
+	 */
+	public Iterator<Field> fields() {
+		// some code goes here
+		return fields.iterator();
+	}
+
+	/**
+	 * reset the TupleDesc of this tuple (only affecting the TupleDesc)
+	 */
+	public void resetTupleDesc(TupleDesc td) {
+		this.tupleDesc = td;
+	}
 }
