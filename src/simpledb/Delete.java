2eda1663b13b11a76c811f0656c2c8bdbb943076
jiangfucheng <1678179319@qq.com>
2019-12-11 16:33:47 +0800

    完成 lab2 exercise4

--- src/simpledb/Delete.java
+++ src/simpledb/Delete.java
@@ -10,6 +10,13 @@ public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private TransactionId transactionId;
+
+    private OpIterator child;
+
+    private TupleDesc resultTupleDesc;
+
+    private boolean called;
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -20,24 +27,30 @@ public class Delete extends Operator {
      *            The child operator from which to read tuples for deletion
      */
     public Delete(TransactionId t, OpIterator child) {
-        // some code goes here
+        this.transactionId = t;
+        this.child = child;
+        Type[] types = new Type[]{Type.INT_TYPE};
+        this.resultTupleDesc = new TupleDesc(types);
+        this.called = false;
     }
 
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+       return this.resultTupleDesc;
     }
 
     public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
+        super.open();
+        child.open();
     }
 
     public void close() {
-        // some code goes here
+        super.close();
+        child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
+        child.rewind();
+        this.called = false;
     }
 
     /**
@@ -50,19 +63,31 @@ public class Delete extends Operator {
      * @see BufferPool#deleteTuple
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-        // some code goes here
-        return null;
+        BufferPool bufferPool = Database.getBufferPool();
+        int count = 0;
+        if(called) return null;
+        while(child.hasNext()){
+            try {
+                bufferPool.deleteTuple(transactionId,child.next());
+                count++;
+            } catch (IOException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        called = true;
+        Tuple result = new Tuple(resultTupleDesc);
+        result.setField(0,new IntField(count));
+        return result;
     }
 
     @Override
     public OpIterator[] getChildren() {
-        // some code goes here
-        return null;
+        return new OpIterator[]{child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
-        // some code goes here
+        this.child = children[0];
     }
 
 }
--- src/simpledb/Insert.java
+++ src/simpledb/Insert.java
@@ -1,74 +1,101 @@
 package simpledb;
 
+import java.io.IOException;
+
 /**
  * Inserts tuples read from the child operator into the tableId specified in the
  * constructor
  */
 public class Insert extends Operator {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Constructor.
-     *
-     * @param t
-     *            The transaction running the insert.
-     * @param child
-     *            The child operator from which to read tuples to be inserted.
-     * @param tableId
-     *            The table in which to insert tuples.
-     * @throws DbException
-     *             if TupleDesc of child differs from table into which we are to
-     *             insert.
-     */
-    public Insert(TransactionId t, OpIterator child, int tableId)
-            throws DbException {
-        // some code goes here
-    }
+	private TransactionId transactionId;
 
-    public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
-    }
+	private OpIterator child;
 
-    public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
-    }
+	private int tableId;
 
-    public void close() {
-        // some code goes here
-    }
+	private TupleDesc tupleDesc;
 
-    public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
-    }
+	private boolean called;
+	/**
+	 * Constructor.
+	 *
+	 * @param t       The transaction running the insert.
+	 * @param child   The child operator from which to read tuples to be inserted.
+	 * @param tableId The table in which to insert tuples.
+	 * @throws DbException if TupleDesc of child differs from table into which we are to
+	 *                     insert.
+	 */
+	public Insert(TransactionId t, OpIterator child, int tableId)
+			throws DbException {
+		this.transactionId = t;
+		this.child = child;
+		this.tableId = tableId;
+		Type[] types = new Type[]{Type.INT_TYPE};
+		this.tupleDesc = new TupleDesc(types);
+		this.called = false;
+	}
 
-    /**
-     * Inserts tuples read from child into the tableId specified by the
-     * constructor. It returns a one field tuple containing the number of
-     * inserted records. Inserts should be passed through BufferPool. An
-     * instances of BufferPool is available via Database.getBufferPool(). Note
-     * that insert DOES NOT need check to see if a particular tuple is a
-     * duplicate before inserting it.
-     *
-     * @return A 1-field tuple containing the number of inserted records, or
-     *         null if called more than once.
-     * @see Database#getBufferPool
-     * @see BufferPool#insertTuple
-     */
-    protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-        // some code goes here
-        return null;
-    }
+	public TupleDesc getTupleDesc() {
+		return this.tupleDesc;
+	}
 
-    @Override
-    public OpIterator[] getChildren() {
-        // some code goes here
-        return null;
-    }
+	public void open() throws DbException, TransactionAbortedException {
+		super.open();
+		child.open();
+	}
 
-    @Override
-    public void setChildren(OpIterator[] children) {
-        // some code goes here
-    }
+	public void close() {
+		super.close();
+		child.close();
+	}
+
+	public void rewind() throws DbException, TransactionAbortedException {
+		child.rewind();
+		called = false;
+	}
+
+	/**
+	 * Inserts tuples read from child into the tableId specified by the
+	 * constructor. It returns a one field tuple containing the number of
+	 * inserted records. Inserts should be passed through BufferPool. An
+	 * instances of BufferPool is available via Database.getBufferPool(). Note
+	 * that insert DOES NOT need check to see if a particular tuple is a
+	 * duplicate before inserting it.
+	 *
+	 * @return A 1-field tuple containing the number of inserted records, or
+	 * null if called more than once.
+	 * @see Database#getBufferPool
+	 * @see BufferPool#insertTuple
+	 */
+	protected Tuple fetchNext() throws TransactionAbortedException, DbException {
+		BufferPool bufferPool = Database.getBufferPool();
+		int count = 0;
+		if(called) return null;
+		while (child.hasNext()) {
+			Tuple next = child.next();
+			try {
+				bufferPool.insertTuple(transactionId, tableId, next);
+				count++;
+			} catch (IOException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		called = true;
+		Tuple res = new Tuple(tupleDesc);
+		res.setField(0, new IntField(count));
+		return res;
+	}
+
+	@Override
+	public OpIterator[] getChildren() {
+		return new OpIterator[]{child};
+	}
+
+	@Override
+	public void setChildren(OpIterator[] children) {
+		this.child = children[0];
+	}
 }
